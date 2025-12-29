2156233f97f3f06484e3eead1a91086b9ebd7bd7
Mike Dunnegan <miked@sincera.io>
2025-12-29 10:00:41 -0500

    rider folder

--- global/Events.gd
+++ global/Events.gd
@@ -2,9 +2,10 @@ extends Node
 @warning_ignore_start("unused_signal")
 
 signal playing_card_toggled(card: PlayingCardUI, selected: bool)
+signal hand_type_changed(hand_type: HandType)
+
 signal hand_played(cards: Array[PlayingCardUI], hand_type: HandType)
 signal hand_discarded(cards: Array[PlayingCardUI])
 
 signal card_scored(card: PlayingCardUI)
-signal hand_type_changed(hand_type: HandType)
 signal hand_scored
